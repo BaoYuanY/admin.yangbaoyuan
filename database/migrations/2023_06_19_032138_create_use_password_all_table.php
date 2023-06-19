ff68ffd73f369eae68b7eca475f931d3322b312d
BaoYuan <baoyuan0304@163.com>
2023-06-19 23:20:08 +0800

    add

---
+++ database/migrations/2023_06_19_032138_create_use_password_all_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsePasswordAllTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('use_password_all', function (Blueprint $table) {
+            $table->id();
+            $table->string('platform', 255)->default('')->comment('平台');
+            $table->string('phone', 15)->default('')->comment('手机');
+            $table->string('email', 32)->default('')->comment('邮箱');
+            $table->string('password', 255)->default('')->comment('密码');
+            $table->string('salt', 32)->default('')->comment('加盐');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('use_password_all');
+    }
+}
