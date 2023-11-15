ff16e1eda99f9a62d0f060213fc815767fc08ec9
baoyuan <yangbaoyuan0304@163.com>
2023-11-15 11:21:11 +0800

    book migrate

--- database/migrations/2023_11_09_151858_create_books_table.php
+++ database/migrations/2023_11_09_151858_create_books_table.php
@@ -22,10 +22,10 @@ class CreateBooksTable extends Migration
             $table->unsignedTinyInteger('source')->default(0)->comment('0-默认 1-纸质 2-neatReader 3-阿里云盘 4-百度网盘 5-Apple 6-BookChat');
             $table->string('type', 12)->default('')->comment('类型 纸质/PDF/EPUB/MOBI');
             $table->string('publisher', 64)->default('')->comment('出版社');
-            $table->string('coverUrl', 64)->default('')->comment('书籍封面图片 URL');
+            $table->string('coverUrl', 128)->default('')->comment('书籍封面图片 URL');
             $table->text('summary')->nullable()->comment('书籍简介');
             $table->unsignedInteger('publishDate')->default(0)->comment('出版日期');
-            $table->unsignedTinyInteger('version')->default(0)->comment('版本');
+            $table->unsignedTinyInteger('version')->default(1)->comment('版本');
             $table->string('language', 32)->default('中文')->comment('原始语言');
             $table->string('category')->default('')->comment('分类');
             $table->unsignedInteger('sort')->default(0)->comment('排序');
